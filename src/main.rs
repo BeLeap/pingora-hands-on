f553f867d3dceaa52f7ba34844c6b35a35de485c
BeLeap <beleap@beleap.dev>
2024-03-11 22:00:41 +0900

    feat: dummy main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use pingora::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_server = Server::new(None).unwrap();
+    my_server.bootstrap();
+    my_server.run_forever();
 }
