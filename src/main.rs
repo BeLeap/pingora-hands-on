aec0e5afb1b4bc4d48600e117391ca295737a071
BeLeap <beleap@beleap.dev>
2024-03-17 18:39:32 +0900

    add invalid addr

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
-    let upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443"]).unwrap();
+    let upstreams =
+        LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443", "127.0.0.1:343"]).unwrap();
 
     let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
     lb.add_tcp("0.0.0.0:6188");
